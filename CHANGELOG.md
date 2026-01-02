f0e38280fe8e0b45b81b861677e8adf50eab9cad
aryadovoy <ryav.omsk@gmail.com>
2026-01-02 11:48:04 +0400

    bump: version 0.1.0 → 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+## 0.2.0 (2026-01-02)
+
+### Feat
+
+- decreased possible python version
+
+### Fix
+
+- updated `Subscription` and `Tag`
+
 ## 0.1.0 (2025-07-28)
 
 ### Feat
--- src/inocli/__init__.py
+++ src/inocli/__init__.py
@@ -1,7 +1,7 @@
 from .client import CustomTag, InoreaderClient, SystemTag
 from .configs import InoreaderConfig
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "InoreaderClient",
